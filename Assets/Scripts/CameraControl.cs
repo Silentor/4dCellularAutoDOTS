70b335be36d0de4b189c5cf3d10a8fe4add852b1
Silentor666 <silentor@users.noreply.github.com>
2025-03-28 07:54:38 +0200

    Improved look around, calculate selected block at MonoBeh side, add hot/cold influence, throw input to ECS https://github.com/Silentor/4dCellularAutoDOTS/issues/3

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -1,38 +1,56 @@
+using System;
+using Unity.Entities;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Object = System.Object;
 
 namespace Core
 {
     public class CameraControl : MonoBehaviour
     {
+        public Vector2 LookAroundSpeed = new Vector2( 0.2f, 0.2f );
+
         private Transform   _cameraTransform;
 
         private Vector3     _eulerRotation;
         private InputAction _lookAroundAction;
         private InputAction _moveAction;
         private InputAction _sprintAction;
+        private Camera _camera;
+        private World _world;
+        private Entity _inputEntity;
+        private InputAction _hotAction;
+        private InputAction _coldAction;
 
         private void Start( )
         {
             _lookAroundAction = InputSystem.actions.FindAction( "Look" );
             _moveAction       = InputSystem.actions.FindAction( "Move" );
             _sprintAction     = InputSystem.actions.FindAction( "Sprint" );
-            _cameraTransform  = Camera.main.transform;
+            _hotAction     = InputSystem.actions.FindAction( "Attack" );
+            _coldAction     = InputSystem.actions.FindAction( "Cold" );
+            _camera = Camera.main;
+            _cameraTransform  = _camera.transform;
             _eulerRotation    = _cameraTransform.rotation.eulerAngles;
+
+            _world = World.DefaultGameObjectInjectionWorld;
         }
 
         private void Update( )
         {
+            //Look around
             var mousePosition = Mouse.current.position.ReadValue();
             if ( !(mousePosition.x < 0) && !(mousePosition.x > Screen.width) &&
                  !(mousePosition.y < 0) && !(mousePosition.y > Screen.height) )
             {
-                var lookDelta          = _lookAroundAction.ReadValue<Vector2>();
+                var lookDelta          = _lookAroundAction.ReadValue<Vector2>() * LookAroundSpeed;
                 _eulerRotation            += new Vector3( -lookDelta.y,                             lookDelta.x,      0 );
                 _eulerRotation            =  new Vector3( Mathf.Clamp( _eulerRotation.x, -45, 45 ), _eulerRotation.y, 0 );
                 _cameraTransform.rotation =  Quaternion.Euler( _eulerRotation );
             }
 
+            //Movement
             var moveDelta     = _moveAction.ReadValue<Vector2>();
             if ( moveDelta != Vector2.zero )
             {
@@ -41,6 +59,47 @@ namespace Core
                     worldMovement *= 10;
                 var localMovement = _cameraTransform.rotation * worldMovement;
                 _cameraTransform.position += localMovement;
+            }
+
+            //Change temperature by mouse
+            var temperatureDiff = 0f;
+            if ( _hotAction.IsPressed() )
+                temperatureDiff += Time.deltaTime;
+            if( _coldAction.IsPressed() )
+                temperatureDiff -= Time.deltaTime;
+
+            //Select cell by mouse
+            int2 selectedCell = int2.zero;
+            bool isSelectedCell = false;
+            var mouseRay = _camera.ScreenPointToRay( mousePosition );
+            var gridPlane = new Plane( Vector3.up, 0 );
+            if( gridPlane.Raycast( mouseRay, out var enter ) )
+            {
+                 var hitPoint = mouseRay.GetPoint( enter );
+                 selectedCell = new int2( Mathf.FloorToInt( hitPoint.x ), Mathf.FloorToInt( hitPoint.z ) );
+                 if( selectedCell.x >= 0 && selectedCell.x < Config.GridSize &&
+                     selectedCell.y >= 0 && selectedCell.y < Config.GridSize )
+                 {
+                     isSelectedCell = true;
+                 }
+            }
+
+            SetInputToECS( isSelectedCell, selectedCell, temperatureDiff );
+        }
+
+        private void SetInputToECS( bool isSelectedCell, int2 selectedCell, float temperatureDiff )
+        {
+            if ( _world.IsCreated )
+            {
+                if ( !_world.EntityManager.Exists( _inputEntity ) )
+                    _inputEntity = _world.EntityManager.CreateSingleton<Input>(  );
+
+                _world.EntityManager.SetComponentData( _inputEntity, new Input()
+                                                                     {
+                                                                             IsSelectedCell = isSelectedCell,
+                                                                             SelectedCell = selectedCell,
+                                                                             TemperatureDiff = temperatureDiff,
+                                                                     } );
             }
         }
     }
---
+++ Assets/Scripts/Input.cs
@@ -0,0 +1,12 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+namespace Core
+{
+    public struct Input : IComponentData
+    {
+        public bool IsSelectedCell;
+        public int2 SelectedCell;
+        public float TemperatureDiff;
+    }
+}
